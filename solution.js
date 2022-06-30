89f5ddbd25964f91f56f7f28e5412e405b0c5f6b
Marcelearn <jaworskimarcel@gmail.com>
2022-06-30 15:05:11 +0200

    erste commit

---
+++ solution.js
@@ -0,0 +1,23 @@
+const apples=20;//0
+const oranges=30;
+
+const applesGleighOranges=apples===oranges;
+const applesNichtGleichOranges=apples!==oranges;
+const applesGreaterOranges=apples>oranges;
+console.log({applesGleighOranges});//1
+console.log({applesNichtGleichOranges});//2
+console.log({applesGreaterOranges});//3
+console.log(apples<=oranges);//4
+console.log(oranges>apples);//5
+const mangos=5;//6
+console.log(mangos*apples>mangos+oranges);
+console.log(apples-mangos<oranges/mangos);//7
+console.log(mangos===apples===oranges);//8
+console.log(apples%mangos===oranges%mangos);//9
+console.log(mangos+apples>=oranges-mangos);//10
+
+
+
+
+
+
