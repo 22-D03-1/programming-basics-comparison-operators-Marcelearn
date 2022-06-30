21894802447d46f807b0fd723ce3881525bbc7c2
Marcelearn <jaworskimarcel@gmail.com>
2022-06-30 15:28:41 +0200

    fertigAufgabe

--- solution.js
+++ solution.js
@@ -4,17 +4,25 @@ const oranges=30;
 const applesGleighOranges=apples===oranges;
 const applesNichtGleichOranges=apples!==oranges;
 const applesGreaterOranges=apples>oranges;
+const applesLessEqualOranges=apples<=oranges;
+const orangesGreaterApples=oranges>apples;
 console.log({applesGleighOranges});//1
 console.log({applesNichtGleichOranges});//2
 console.log({applesGreaterOranges});//3
-console.log(apples<=oranges);//4
-console.log(oranges>apples);//5
+console.log({applesLessEqualOranges});//4
+console.log({orangesGreaterApples});//5
 const mangos=5;//6
-console.log(mangos*apples>mangos+oranges);
-console.log(apples-mangos<oranges/mangos);//7
-console.log(mangos===apples===oranges);//8
-console.log(apples%mangos===oranges%mangos);//9
-console.log(mangos+apples>=oranges-mangos);//10
+const exercise6=mangos*apples>mangos+oranges;
+console.log({exercise6});
+const exercise7=apples-mangos<oranges/mangos;
+console.log({exercise7});//7
+const exercise8=mangos===apples===oranges;
+console.log({exercise8});//8
+const exercise9=apples%mangos===oranges%mangos;
+console.log({exercise9});//9
+const exercise10=mangos+apples>=oranges-mangos;
+console.log({exercise10});//10
+
 
 
 
